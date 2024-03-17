fee620f4f02412c2725b6ef16821dd810ce62721
Alla Magina <babara@flylady.su>
2024-03-17 22:25:26 +0300

    correct eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,8 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:prettier/recommended',
   ],
